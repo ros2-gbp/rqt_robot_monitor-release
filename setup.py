a02cf9f26c8847b06f6799e322f980bc008ae07a
Arne Hitzmann <arne.hitzmann@gmail.com>
2024-05-15 18:20:03 +0900

    Rebase from 'debian/humble/rqt_robot_monitor'

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python
-
 from setuptools import setup
 
 package_name = 'rqt_robot_monitor'
-version = '1.0.5'
 
 setup(
-    name = package_name,
-    version = version,
-    packages = [package_name],
-    package_dir = {'': 'src'},
-    data_files = [
+    name=package_name,
+    version='1.0.6',
+    packages=[package_name],
+    package_dir={'': 'src'},
+    data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -20,32 +17,29 @@ setup(
         ('share/' + package_name + '/resource',
             ['resource/timelinepane.ui'])
     ],
-    install_requires = ['setuptools'],
-    zip_safe = True,
-    author = "Austin Hendrix, Isaac Saito, Ze'Ev Klapow, Josh Faust",
-    maintainer = "Aaron Blasdel, Arne Hitzmann, Austin Hendrix",
-    maintainer_email = "ablasdel@gmail.com, arne.hitzmann@gmail.com, namniart@gmail.com",
-    keywords = ["ROS 2"],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author="Austin Hendrix, Isaac Saito, Ze'Ev Klapow, Josh Faust, Aaron Blasdel",
+    maintainer="Arne Hitzmann, Austin Hendrix",
+    maintainer_email="arne.hitzmann@gmail.com, namniart@gmail.com",
+    keywords=["ROS 2"],
     classifiers = [
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description = (
+    description=(
         'rqt_robot_monitor displays diagnostics_agg topics messages that \
         are published by "http://www.ros.org/wiki/diagnostic_aggregator" (diagnostic_aggregator). \
         rqt_robot_monitor is a direct port to rqt of \
         "http://www.ros.org/wiki/robot_monitor" (robot_monitor).'
     ),
-    license = 'BSD',
-    tests_require = ['pytest'],
-    entry_points = {
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
         'console_scripts': [
             'rqt_robot_monitor = rqt_robot_monitor.robot_monitor:main'
         ]
     }
-
-
-
 )
